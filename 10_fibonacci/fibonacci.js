b2b046b83c6daa438101986b12f9c43e3a06156d
Muhammad Ahmad <69284724+Ahmad1786@users.noreply.github.com>
2023-05-09 22:33:48 -0400

    fix memoization error

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -3,7 +3,7 @@ const fibonacci = function(n, memo={}) {
     if (+n < 0 ) {return 'OOPS'}
     if (+n < 2 ) {return +n}
 
-    memo[+n] = fibonacci(+n-1) + fibonacci(+n-2)
+    memo[+n] = fibonacci(+n-1, memo) + fibonacci(+n-2, memo)
     return memo[+n]
 };
 
