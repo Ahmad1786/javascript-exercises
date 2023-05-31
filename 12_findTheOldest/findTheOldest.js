f5795bdf69b8e42eddfa0c5f3357322545f67236
Muhammad Ahmad <69284724+Ahmad1786@users.noreply.github.com>
2023-05-30 20:12:35 -0400

    finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
 const findTheOldest = function(array) {
-    // will finish one day!!!
+    let endingYearFirst = ("yearOfDeath" in array[0]) ? array[0]["yearOfDeath"] : new Date().getFullYear();
+    let holder = endingYearFirst - array[0]["yearOfBirth"];
+    
+    return array.reduce((obj, elem) => {
+        let endingYear = ("yearOfDeath" in elem) ? elem["yearOfDeath"] : new Date().getFullYear();
+        let age = endingYear - elem["yearOfBirth"];
+        if (age > holder) {
+            holder = age;
+            obj = elem;
+        }
+        return obj;
+    }, array[0]);
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
